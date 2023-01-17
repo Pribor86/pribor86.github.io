142c514b87a8eb9ec96bab3b9f0d25fda23a5c4a
Aleksandr Grigorjev <pribor86@gmail.com>
2023-01-17 17:02:11 +0200

    check

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import '../styles/backButton.scss';
-
 interface IBackButtonProps {
     openInfoCard: () => void;
 }
-
 export const BackButton: React.FC<IBackButtonProps> = (props) => {
-
     return (
         <div className='back-button' onClick={props.openInfoCard}>
             <div className='back-button-label'>
