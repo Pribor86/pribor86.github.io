297467224fcfe90055f635a66a556ce87bde4e16
Aleksandr Grigorjev <pribor86@gmail.com>
2023-01-30 21:33:02 +0200

    update

--- src/store/setSelectedDropdownItemId.ts
+++ src/store/setSelectedDropdownItemId.ts
@@ -5,15 +5,16 @@ const selectedDropdownItemIdState: SelectedDropdownItemIdState = {
     selectedDropdownItemId: '',
 }
 
-const selectedDropdownItemIdReducer = (state = selectedDropdownItemIdState, action: SelectedDropdownItemIdAction) => {
-    switch (action.type) {
-        case actionTypes.SET_SELECTED_DROPDOWN_ITEM_ID:
-            return {
-                selectedDropdownItemId: action.id
-            }
-        default:
-            return state
+const selectedDropdownItemIdReducer =
+    (state = selectedDropdownItemIdState, action: SelectedDropdownItemIdAction) => {
+        switch (action.type) {
+            case actionTypes.SET_SELECTED_DROPDOWN_ITEM_ID:
+                return {
+                    selectedDropdownItemId: action.id
+                }
+            default:
+                return state
+        }
     }
-}
 
 export default selectedDropdownItemIdReducer
